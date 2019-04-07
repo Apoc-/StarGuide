6d24526e660e72d3313c2523b684e45be1cb87fa
René Ziegler <ziegler.rene87@gmx.de>
2019-04-07 16:57:42 +0200

    switched to SuperTilemapEditor

---
+++ Assets/Scripts/Behaviour/GameManager.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+namespace Behaviour
+{
+    public class GameManager : MonoBehaviour
+    {
+        private static GameManager _instance;
+
+        public static GameManager Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = FindObjectOfType<GameManager>();
+                }
+                return _instance;
+            }
+        }
+
+        public GameObject Player;
+    }
+}
--- Assets/Scripts/Behaviour/World/DoorBehaviour.cs
+++ Assets/Scripts/Behaviour/World/DoorBehaviour.cs
@@ -1,18 +1,57 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using Behaviour;
+using Behaviour.World;
+using CreativeSpore.SuperTilemapEditor;
 using UnityEngine;
 
+[RequireComponent(typeof(Collider2D))]
 public class DoorBehaviour : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Vector2 DoorLocation;
+    private Collider2D _doorTrigger;
+    public STETilemap DoorTilemap;
+
+    private bool _isOpen;
+
+    private void Start()
     {
-        
+        _doorTrigger = GetComponent<Collider2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
+        var playerCollider = GameManager.Instance.Player.GetComponent<Collider2D>();
         
+        if(_doorTrigger.IsTouching(playerCollider))
+        {
+            _isOpen = true;
+        }
+        else
+        {
+            _isOpen = false;
+        }
+
+        SetDoorState(_isOpen);
+    }
+
+    void SetDoorState(bool state)
+    {
+        
+        var bot = DoorLocation;
+        var top = new Vector2(bot.x, bot.y + 1);
+
+        if (!state)
+        {
+            DoorTilemap.SetTile(bot, 199);
+            DoorTilemap.SetTile(top, 167);
+        }
+        else
+        {
+            DoorTilemap.Erase(bot);
+            DoorTilemap.Erase(top);
+        }
+        
+        DoorTilemap.UpdateMeshImmediate();
     }
 }
