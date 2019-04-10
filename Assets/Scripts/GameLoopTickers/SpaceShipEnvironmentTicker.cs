131b28fd55090341c7390b5110fbd71da25176e7
Mijago <coding.mijago@gmail.com>
2019-04-10 20:36:56 +0200

    SpaceShipEnvironmentTicker now slowly generates dirt. It's dirty magic!

--- Assets/Scripts/GameLoopTickers/SpaceShipEnvironmentTicker.cs
+++ Assets/Scripts/GameLoopTickers/SpaceShipEnvironmentTicker.cs
@@ -13,8 +13,11 @@ namespace SpaceShip
         private readonly STETilemap _shipBackgroundTilemap;
 
         private List<Vector2> _dirtiableTiles = new List<Vector2>();
-        private readonly List<int> _dirtTiles = new List<int> {224, 225, 226, 227, 228, 229};
+        private readonly List<int> _dirtTiles = new List<int> {64, 96, 128};
         private readonly STETilemap _doodadsBackgroundTilemap;
+
+        private int _tickCounter = 0;
+
 
         public SpaceShipEnvironmentTicker()
         {
@@ -33,7 +36,12 @@ namespace SpaceShip
 
         public void Tick()
         {
-            TryToPlaceDirt();
+            _tickCounter++;
+            if (_tickCounter % 30 == 0)
+            {
+                _tickCounter = 0;
+                TryToPlaceDirt();
+            }
         }
 
         private void SetDirtiableTiles()
@@ -67,13 +75,14 @@ namespace SpaceShip
                 if (Tileset.k_TileData_Empty == tileData)
                 {
                     freePositions.Add(tileVector);
-                } else if (IsDirtTile(tileData))
+                }
+                else if (IsDirtTile(tileData))
                 {
                     dirtyCount++;
                 }
             }
-            
-            if (freePositions.Count == 0 || dirtyCount >= 5)
+
+            if (freePositions.Count == 0)
                 return null;
 
             var toSkip = MathHelper.GetRandomInt(0, freePositions.Count);
@@ -89,12 +98,44 @@ namespace SpaceShip
 
         private void TryToPlaceDirt()
         {
+
+            if (MathHelper.GetRandomInt(0, 100) < 80)
+                return;
+            
             var randomTile = GetRandomPlaceableTilePosition();
             if (randomTile.HasValue)
             {
-                Debug.Log("Place Dirt at "+randomTile.Value);
-                _doodadsBackgroundTilemap.SetTile(randomTile.Value, GetRandomDirtTile());
+                var flag = GetRandomTileRotationFlag();
+
+                _doodadsBackgroundTilemap.SetTile(
+                    randomTile.Value,
+                    GetRandomDirtTile(),
+                    Tileset.k_BrushId_Default,
+                    flag
+                );
+                _doodadsBackgroundTilemap.UpdateMesh();
             }
+        }
+
+        private static eTileFlags GetRandomTileRotationFlag()
+        {
+            eTileFlags flag = 0;
+            if (MathHelper.GetRandomInt(0, 10) > 5)
+            {
+                flag &= eTileFlags.Rot90;
+            }
+
+            if (MathHelper.GetRandomInt(0, 10) > 5)
+            {
+                flag &= eTileFlags.FlipH;
+            }
+
+            if (MathHelper.GetRandomInt(0, 10) > 5)
+            {
+                flag &= eTileFlags.FlipV;
+            }
+
+            return flag;
         }
     }
 }
