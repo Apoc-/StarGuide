32bb1db6feeaa2d69a5d4340755f730d3ac51bd3
Mijago <coding.mijago@gmail.com>
2019-04-14 17:25:31 +0200

    Fixed Buildscript

--- Assets/Editor/BuildWindows.cs
+++ Assets/Editor/BuildWindows.cs
@@ -1,6 +1,4 @@
 using UnityEditor;
-using UnityEditor.Build.Reporting;
-using UnityEngine;
 
 namespace Editor
 {
@@ -10,23 +8,12 @@ namespace Editor
         public static void BuildWindows()
         {
             BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
-            buildPlayerOptions.scenes = new[] { "Assets/Scenes/GameScene.unity" };
+            buildPlayerOptions.scenes = new[] {"Assets/Scenes/GameScene.unity"};
             buildPlayerOptions.locationPathName = "./dist/StarGuide.exe";
             buildPlayerOptions.target = BuildTarget.StandaloneWindows;
             buildPlayerOptions.options = BuildOptions.None;
 
-            BuildReport report = BuildPipeline.BuildPlayer(buildPlayerOptions);
-            BuildSummary summary = report.summary;
-
-            if (summary.result == BuildResult.Succeeded)
-            {
-                Debug.Log("Build succeeded: " + summary.totalSize + " bytes");
-            }
-
-            if (summary.result == BuildResult.Failed)
-            {
-                Debug.Log("Build failed");
-            }
+            BuildPipeline.BuildPlayer(buildPlayerOptions);
         }
     }
 }
